00ac55dbf351fb77f7b8690412fb4bfdd74fe451
GianAndrea Müller <gam1994@gmail.com>
2020-02-03 13:00:43 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,10 @@ sudo apt-get dist-upgrade
 # Install sensirion specific packages
 pip install sensirion-shdlc-driver
 pip install sensirion-shdlc-sensorbridge
+
+# Install pycharm
+
 # Enable tty-support for the sensor bridge
+# The file below contains the instructions what do to when an SEK-Sensorbridge is connected
+# It triggers the installation of the corresponding driver and assigns a tty-port to the device.
+cp 99-sensorbridge.rules /etc/udev/rules.d
