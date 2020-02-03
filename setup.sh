9dc34d43c6a1e5a9cb186c7fbf85180960179124
GianAndrea Mueller <gam1994@gmail.com>
2020-02-03 17:02:02 +0100

    Make setup script output readable.

--- setup.sh
+++ setup.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
+
+# Definitions
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 # Setup script
+echo -e "${RED}Updating the distribution...${NC}"
 sudo apt-get update
 sudo apt-get dist-upgrade
+echo -e "${GREEN}Done!${NC}"
+
 
 # Install sensirion specific packages
+echo -e "${RED}Installing sensirion specific drivers...${NC}"
 pip install sensirion-shdlc-driver
 pip install sensirion-shdlc-sensorbridge
+echo -e "${GREEN}Done!${NC}"
 
 # Install pycharm
-wget https://download.jetbrains.com/python/pycharm-community-2019.3.2.tar.gz -P ~/Downloads
+echo -e "${RED}Downloading and installing pycharm...${NC}"
+wget -nc https://download.jetbrains.com/python/pycharm-community-2019.3.2.tar.gz -P ~/Downloads
 sudo tar xfz ~/Downloads/pycharm-*.tar.gz -C /opt/
+echo -e "${GREEN}Done!${NC}"
 
 # Enable tty-support for the sensor bridge
 # The file below contains the instructions what do to when an SEK-Sensorbridge is connected
 # It triggers the installation of the corresponding driver and assigns a tty-port to the device.
-cp 99-sensorbridge.rules /etc/udev/rules.d
+echo -e "${RED}Setting up USB devices...${NC}"
+sudo cp 99-sensorbridge.rules /etc/udev/rules.d
+echo -e "${GREEN}Done!${NC}"
 # sudo /bin/sh -c 'echo 0403 7168 >/sys/bus/usb-serial/drivers/ftdi_sio/new_id'
