43115540be777cad1909fc8eeb12323476151e8b
GianAndrea Müller <gam1994@gmail.com>
2020-02-19 15:19:58 +0100

    Update setup scripts

---
+++ 01_SETUP/tty_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Definitions
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# Enable tty-support for the sensor bridge
+# The file below contains the instructions what do to when an SEK-Sensorbridge is connected
+# It triggers the installation of the corresponding driver and assigns a tty-port to the device.
+echo -e "${RED}Setting up USB devices...${NC}"
+sudo cp 99-sensorbridge.rules /etc/udev/rules.d
+echo -e "${GREEN}Done!${NC}"
+sudo /bin/sh -c 'echo 0403 7168 >/sys/bus/usb-serial/drivers/ftdi_sio/new_id'
