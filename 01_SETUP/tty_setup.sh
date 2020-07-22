de8aef9408fc34d612f52b1606d18c148181568e
GianAndrea Müller <gam1994@gmail.com>
2020-07-22 14:55:50 +0200

    Make tty setup location independent

--- 01_SETUP/tty_setup.sh
+++ 01_SETUP/tty_setup.sh
@@ -5,10 +5,15 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+# get current script location
+full_path=$(realpath $0)
+dir_path=$(dirname $full_path)
+
 # Enable tty-support for the sensor bridge
 # The file below contains the instructions what do to when an SEK-Sensorbridge is connected
 # It triggers the installation of the corresponding driver and assigns a tty-port to the device.
 echo -e "${RED}Setting up USB devices...${NC}"
-sudo cp 99-sensorbridge.rules /etc/udev/rules.d
+# Use current script location to retrieve local file
+sudo cp $dir_path/99-sensorbridge.rules /etc/udev/rules.d
 echo -e "${GREEN}Done!${NC}"
 sudo /bin/sh -c 'echo 0403 7168 >/sys/bus/usb-serial/drivers/ftdi_sio/new_id'
