f387d35e78d7acb4384b722180b437c355572391
muellegi <muellegi@student.ethz.ch>
2020-08-12 15:30:10 +0200

    Reformating with black

--- 02_SOFTWARE/main.py
+++ 02_SOFTWARE/main.py
@@ -3,19 +3,12 @@ from Utility.Logger import setup_custom_logger
 from logging import getLevelName
 from GUI.ExperimentOverview import ExperimentOverview
 
-logger = setup_custom_logger(name='root', level=getLevelName('WARNING'))
+logger = setup_custom_logger(name="root", level=getLevelName("WARNING"))
 
 if __name__ == "__main__":
-    serials = {
-        'EKS': 'EKS23Z8C1I',
-        'SFC': 'FT1PXV63',
-        'Heater': 'AM01ZB7J'
-    }
-    with Setup(serials=serials) as setup:
+    serials = {"EKS": "EKS23Z8C1I", "SFC": "FT1PXV63", "Heater": "AM01ZB7J"}
+    with Setup(serials=serials, t_sampling_s=0.25, interval_s=50) as setup:
         setup.open()
-        setup.start_measurement_thread(t_sampling_sec=0.3)
+        setup.start_measurement_thread()
         # Start up GUI
         GUI = ExperimentOverview(setup=setup)
-
-
-
