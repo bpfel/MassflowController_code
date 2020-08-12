dbcac50b0f39e513059354c242e3b60bb65fb757
GianAndrea Müller <muellegi@student.ethz.ch>
2020-08-12 13:27:36 +0200

    Dimension buffer according to monitored time interval and sampling time

--- 02_SOFTWARE/Utility/MeasurementBuffer.py
+++ 02_SOFTWARE/Utility/MeasurementBuffer.py
@@ -1,18 +1,20 @@
 import logging
 from collections import deque
 
-logger = logging.getLogger('root')
+logger = logging.getLogger("root")
+
 
 class MeasurementBuffer(object):
-    def __init__(self, signals, buffer_length):
+    def __init__(self, signals, sampling_time_s, buffer_interval_s):
         self._signals = signals
         self._data = dict()
+        buffer_length = int(buffer_interval_s / sampling_time_s)
         for signal in signals:
             self._data[signal] = deque(maxlen=buffer_length)
 
     def update(self, measurement: dict):
         if set(measurement.keys()) != set(self._signals):
-            logger.error('Incorrect set of signals supplied!')
+            logger.error("Incorrect set of signals supplied!")
         for signal, value in measurement.items():
             self._data[signal].append(value)
 
@@ -20,4 +22,4 @@ class MeasurementBuffer(object):
         if item in self._signals:
             return self._data[item]
         else:
-            raise KeyError('Signal {} is not available!'.format(item))
+            raise KeyError("Signal {} is not available!".format(item))
