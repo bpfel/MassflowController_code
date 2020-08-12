fa7a9f3aa920582f3a6caaf1ea580d279a2ccdda
GianAndrea Müller <muellegi@student.ethz.ch>
2020-08-12 13:25:12 +0200

    Correct timing of plots

--- 02_SOFTWARE/GUI/ExperimentOverview.py
+++ 02_SOFTWARE/GUI/ExperimentOverview.py
@@ -9,16 +9,17 @@ from collections import deque
 import time
 
 matplotlib.use("TkAgg")
+UPDATE_TIME = 100
 
 
-class ExperimentOverview():
+class ExperimentOverview:
     def __init__(self, setup: Setup):
         self.figures = {
-            'diagnostic_temperatures': None,
-            'diagnostic_flows': None,
-            'diagnostic_delta_temperatures': None,
-            'diagnostic_PWM': None,
-            'graph': None,
+            "diagnostic_temperatures": None,
+            "diagnostic_flows": None,
+            "diagnostic_delta_temperatures": None,
+            "diagnostic_PWM": None,
+            "graph": None,
         }
         self.axes = deepcopy(self.figures)
         self.setup = setup
@@ -28,99 +29,162 @@ class ExperimentOverview():
             self.axes[figure_name] = ax
         self.app = TkGUI(figures=self.figures, setup=setup)
 
-        temp = DoubleLiveMeasurement(ax=self.axes['diagnostic_temperatures'], title='Temperatures',
-                                     ylabel='Temperature [deg C]', ylims=(20, 40),
-                                     signal_buffer_one=setup.measurement_buffer['Temperature 1'],
-                                     signal_buffer_two=setup.measurement_buffer['Temperature 2'],
-                                     time_buffer=setup.measurement_buffer['Time'])
-        self.ani_2 = animation.FuncAnimation(fig=self.figures['diagnostic_temperatures'], func=temp, blit=True,
-                                             interval=100)
+        # temp = DoubleLiveMeasurement(
+        #     ax=self.axes["diagnostic_temperatures"],
+        #     title="Temperatures",
+        #     ylabel="Temperature [deg C]",
+        #     ylims=(20, 40),
+        #     signal_buffer_one=setup.measurement_buffer["Temperature 1"],
+        #     signal_buffer_two=setup.measurement_buffer["Temperature 2"],
+        #     time_buffer=setup.measurement_buffer["Time"],
+        #     line_styles=["b-", "k-"],
+        # )
+        # self.ani_2 = animation.FuncAnimation(
+        #     fig=self.figures["diagnostic_temperatures"],
+        #     func=temp,
+        #     blit=True,
+        #     interval=UPDATE_TIME,
+        # )
 
-        flow = DoubleLiveMeasurement(ax=self.axes['diagnostic_flows'], title='Flow',
-                                     ylabel='Flow [slm]', ylims=(0, 100),
-                                     signal_buffer_one=setup.measurement_buffer['Flow'],
-                                     signal_buffer_two=setup.measurement_buffer['Flow Estimate'],
-                                     time_buffer=setup.measurement_buffer['Time'])
-        self.ani_3 = animation.FuncAnimation(fig=self.figures['diagnostic_flows'], func=flow, blit=True, interval=100)
+        flow = DoubleLiveMeasurement(
+            ax=self.axes["diagnostic_flows"],
+            title="Flow",
+            ylabel="Flow [slm]",
+            ylims=(0, 100),
+            signal_buffer_one=setup.measurement_buffer["Flow"],
+            signal_buffer_two=setup.measurement_buffer["Flow Estimate"],
+            time_buffer=setup.measurement_buffer["Time"],
+            line_styles=["b-", "k-"],
+            interval=self.setup.interval_s,
+        )
+        self.ani_3 = animation.FuncAnimation(
+            fig=self.figures["diagnostic_flows"],
+            func=flow,
+            blit=True,
+            interval=UPDATE_TIME,
+        )
 
-        delta_t = SingleLiveMeasurement(ax=self.axes['diagnostic_delta_temperatures'],
-                                        title='Temperature Difference', ylabel='dT [deg C]', ylims=(0, 20),
-                                        signal_buffer=setup.measurement_buffer['Temperature Difference'],
-                                        time_buffer=setup.measurement_buffer['Time'])
+        delta_t = DoubleLiveMeasurement(
+            ax=self.axes["diagnostic_delta_temperatures"],
+            title="Temperature Difference",
+            ylabel="dT [deg C]",
+            ylims=(0, 20),
+            signal_buffer_one=setup.measurement_buffer["Temperature Difference"],
+            signal_buffer_two=setup.measurement_buffer["Target Delta T"],
+            time_buffer=setup.measurement_buffer["Time"],
+            line_styles=["k-", "r-"],
+            interval=self.setup.interval_s
+        )
 
-        self.ani_4 = animation.FuncAnimation(fig=self.figures['diagnostic_delta_temperatures'], func=delta_t, blit=True,
-                                             interval=100)
+        self.ani_4 = animation.FuncAnimation(
+            fig=self.figures["diagnostic_delta_temperatures"],
+            func=delta_t,
+            blit=True,
+            interval=UPDATE_TIME,
+        )
 
-        pwm = SingleLiveMeasurement(ax=self.axes['diagnostic_PWM'], title='Power',
-                                    ylabel='PWM', ylims=(0, 1),
-                                    signal_buffer=setup.measurement_buffer['PWM'],
-                                    time_buffer=setup.measurement_buffer['Time'])
-        self.ani_5 = animation.FuncAnimation(fig=self.figures['diagnostic_PWM'], func=pwm, blit=True, interval=100)
+        # pwm = SingleLiveMeasurement(
+        #     ax=self.axes["diagnostic_PWM"],
+        #     title="Power",
+        #     ylabel="PWM",
+        #     ylims=(0, 1),
+        #     signal_buffer=setup.measurement_buffer["PWM"],
+        #     time_buffer=setup.measurement_buffer["Time"],
+        # )
+        # self.ani_5 = animation.FuncAnimation(
+        #     fig=self.figures["diagnostic_PWM"], func=pwm, blit=True, interval=UPDATE_TIME
+        # )
         self.app.run()
 
 
 class DoubleLiveMeasurement:
-    def __init__(self, ax, title, ylabel, ylims, signal_buffer_one, signal_buffer_two, time_buffer):
-        self.line_one, = ax.plot([], [], 'k-')
-        self.line_two, = ax.plot([], [], 'b-')
+    def __init__(
+            self,
+            ax,
+            title,
+            ylabel,
+            ylims,
+            signal_buffer_one,
+            signal_buffer_two,
+            time_buffer,
+            line_styles,
+            interval,
+    ):
+        (self.line_one,) = ax.plot([], [], line_styles[0])
+        (self.line_two,) = ax.plot([], [], line_styles[1])
         self.axis = ax
+        self.interval = interval
         self.time_buffer = time_buffer
         self.signal_buffer_one = signal_buffer_one
         self.signal_buffer_two = signal_buffer_two
 
         # Set the axis and plot titles
         self.axis.set_title(title)
-        self.axis.set_xlabel('Time [s]')
+        self.axis.set_xlabel("Time [s]")
         self.axis.set_ylabel(ylabel)
 
         # Set axis limits
-        self.axis.set_xlim(0, 20)
+        self.axis.set_xlim(0, self.interval)
         self.axis.set_ylim(ylims)
 
     def __call__(self, i):
         if i == 0:
             self.line_one.set_data([], [])
             self.line_two.set_data([], [])
-            return self.line_one, self.line_two,
-        t = list(self.time_buffer)
-        y_1 = list(self.signal_buffer_one)
-        y_2 = list(self.signal_buffer_two)
-        self.line_one.set_data(t, y_1)
-        self.line_two.set_data(t, y_2)
-        self.axis.set_xlim(min(t), max(t) + 1.0)
-        return self.line_one, self.line_two,
+            return (
+                self.line_one,
+                self.line_two,
+            )
+        t = np.array(self.time_buffer) - self.time_buffer[-1] + self.interval
+        y_1 = np.array(self.signal_buffer_one)
+        y_2 = np.array(self.signal_buffer_two)
+        chosen_indices = t >= 0
+        t_chosen = t[chosen_indices]
+        y_1_chosen = y_1[chosen_indices]
+        y_2_chosen = y_2[chosen_indices]
+        self.line_one.set_data(t_chosen, y_1_chosen)
+        self.line_two.set_data(t_chosen, y_2_chosen)
+        return (
+            self.line_one,
+            self.line_two,
+        )
 
 
 class SingleLiveMeasurement:
-    def __init__(self, ax, title, ylabel, ylims, signal_buffer, time_buffer):
-        self.line, = ax.plot([], [], 'k-')
+    def __init__(self, ax, title, ylabel, ylims, signal_buffer, time_buffer, interval):
+        (self.line,) = ax.plot([], [], "k-")
+        self.interval = interval
         self.axis = ax
         self.time_buffer = time_buffer
         self.signal_buffer = signal_buffer
 
         # Set the axis and plot titles
         self.axis.set_title(title)
-        self.axis.set_xlabel('Time [s]')
+        self.axis.set_xlabel("Time [s]")
         self.axis.set_ylabel(ylabel)
 
         # Set axis limits
-        self.axis.set_xlim(0, 20)
+        self.axis.set_xlim(0, interval)
         self.axis.set_ylim(ylims)
 
     def __call__(self, i):
         if i == 0:
             self.line.set_data([], [])
-            return self.line,
-        t = list(self.time_buffer)
-        y = list(self.signal_buffer)
-        self.line.set_data(t, y)
+            return (self.line,)
+        initial_time = self.time_buffer[-1] - self.interval
+        t = np.array(self.time_buffer)
+        y = np.array(self.signal_buffer)
+        chosen_indices = t >= initial_time
+        t_chosen = t[chosen_indices]
+        y_chosen = y[chosen_indices]
+        self.line.set_data(t_chosen, y_chosen)
         self.axis.set_xlim(min(t), max(t) + 1.0)
-        return self.line,
+        return (self.line,)
 
 
 class SingleDummyLiveMeasurement:
     def __init__(self, ax, title, ylabel, ylims):
-        self.line, = ax.plot([], [], 'k-')
+        (self.line,) = ax.plot([], [], "k-")
         self.axis = ax
         self.xList = deque(maxlen=50)
         self.yList = deque(maxlen=50)
@@ -130,7 +194,7 @@ class SingleDummyLiveMeasurement:
 
         # Set the axis and plot titles
         self.axis.set_title(title)
-        self.axis.set_xlabel('Time [s]')
+        self.axis.set_xlabel("Time [s]")
         self.axis.set_ylabel(ylabel)
 
         # Set axis limits
@@ -143,10 +207,10 @@ class SingleDummyLiveMeasurement:
     def __call__(self, i):
         if i == 0:
             self.line.set_data([], [])
-            return self.line,
+            return (self.line,)
         t = time.time()
         self.xList.append(t - self.initial_time)
         self.yList.append(np.random.rand() - np.random.rand())
         self.line.set_data(list(self.xList), list(self.yList))
         self.axis.set_xlim(min(self.xList), max(self.xList) + 1.0)
-        return self.line,
+        return (self.line,)
